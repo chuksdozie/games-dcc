ba27f165e06a24df29d608b4df1dfc82172f5ce9
chuks dozie <chuksdozie48@gmail.com>
2025-01-21 15:39:21 +0100

    SEO

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,6 +1,6 @@
 // next-seo.config.js
-const title = "Anonymous - Engage with True Anonymity";
-const description = "Communicate securely and anonymously in real-time.";
+const title = "Games - FC 25";
+const description = "Keeping track of friendly games in a fun way.";
 
 export default {
   title,
@@ -8,18 +8,18 @@ export default {
   openGraph: {
     type: "website",
     locale: "en_US",
-    url: "https://ayf-anonymous.vercel.app/", // Replace with your event's URL
+    url: "https://games.devchuks.com/", // Replace with your event's URL
     title,
     description,
     images: [
       {
-        url: "https://ik.imagekit.io/akf2tcskl/pngegg%20(29)_zEhlifDkh.png?updatedAt=1724186047623", // Replace with your event's banner image URL
+        url: "https://keyrounds.tn/_next/image?url=https%3A%2F%2Fmedia.keyrounds.tn%2Fpublic%2Fmedia%2Fgames%2F309bd940-cbd3-444c-ab0a-ac6f9e38528d.webp&w=828&q=100", // Replace with your event's banner image URL
         width: 800,
         height: 800,
         alt: "Anonymous",
       },
     ],
-    site_name: "Anonymous",
+    site_name: "Games",
   },
   twitter: {
     // handle: "@timeout_event", // Replace with your Twitter handle
